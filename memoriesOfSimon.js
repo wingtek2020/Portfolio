9085ed62f885646af04454e3927a6b47eeaf080c
Christine Wingfield <wingtek2020@gmail.com>
2023-10-14 11:37:43 -0400

    inital commit

---
+++ memoriesOfSimon.js
@@ -0,0 +1,21 @@
+function generateCards() {
+  for (let card of cards) {
+   //display cards randomly to the user
+  }
+}
+
+function flipCard() {
+  // turn all cards to backCard asset or backover to originalCard
+  }
+
+function startGame() {
+  //generate the game set vars
+}
+
+function restart() {
+    //restart the game  
+}
+
+function displayOutcome{
+    //display if user is correct
+}
