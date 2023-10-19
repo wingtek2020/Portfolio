67f509f280813c72f214acf94fa9bff7191b59cb
Austin Jasay <jasayaustin@gmail.com>
2023-10-18 21:41:41 -0400

    added a flip function to the cards on click

--- memoriesOfSimon.js
+++ memoriesOfSimon.js
@@ -3,6 +3,7 @@ let colors = ['blue', 'red', 'yellow', 'green'];
 
 class Card {
   constructor(color) {
+    this.canFlip = true;
     this.color = color;
     this.img = `${color}Card.png`;
     this.render();
@@ -13,7 +14,31 @@ class Card {
     this.element = document.createElement('div');
     this.element.style.backgroundImage = `url("assets/backCard.png")`;
     this.element.classList.add('card');
+    this.element.addEventListener('click', this.flip);
     document.body.appendChild(this.element);
+  };
+
+  flip = () => {
+    // Ensures the card cannot be flipped while it is still in the process of flipping, which would break the animation.
+    if (this.canFlip) {
+      this.canFlip = false;
+      setTimeout(() => {
+        this.canFlip = true;
+      }, 900);
+
+      // Switches the background t and adds or removes the 'flipped' css class which handles the flipping animation
+      let background = '';
+      if (this.element.classList.contains('flipped')) {
+        this.element.classList.remove('flipped');
+        background = 'url(assets/backCard.png)';
+      } else {
+        this.element.classList.add('flipped');
+        background = `url(assets/${this.img})`;
+      }
+      setTimeout(() => {
+        this.element.style.backgroundImage = background;
+      }, 400);
+    }
   };
 }
 
@@ -22,7 +47,7 @@ generateCards(colors);
 function generateCards(colors) {
   let leng = colors.length;
   for (let i = 0; i < leng; i++) {
-    let newCard = new Card(colors[i]);
+    new Card(colors[i]);
   }
 }
 
