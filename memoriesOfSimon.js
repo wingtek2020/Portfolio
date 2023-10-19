71136959f25d340282df0ab2ba235c9c2280ff78
Austin Jasay <jasayaustin@gmail.com>
2023-10-18 21:24:46 -0400

    added card object that renders an element to dom on instantiation

--- memoriesOfSimon.js
+++ memoriesOfSimon.js
@@ -1,23 +1,43 @@
-function generateCards() {
-  for (let card of cards) {
-   //display cards randomly to the user
+let cards = [];
+let colors = ['blue', 'red', 'yellow', 'green'];
+
+class Card {
+  constructor(color) {
+    this.color = color;
+    this.img = `${color}Card.png`;
+    this.render();
+    cards.push(this);
+  }
+
+  render = () => {
+    this.element = document.createElement('div');
+    this.element.style.backgroundImage = `url("assets/backCard.png")`;
+    this.element.classList.add('card');
+    document.body.appendChild(this.element);
+  };
+}
+
+generateCards(colors);
+
+function generateCards(colors) {
+  let leng = colors.length;
+  for (let i = 0; i < leng; i++) {
+    let newCard = new Card(colors[i]);
   }
 }
 
 function flipCard() {
   // turn all cards to backCard asset or backover to originalCard
-  }
+}
 
 function startGame() {
   //generate the game set vars
 }
 
 function restart() {
-    //restart the game  
+  //restart the game
 }
 
-function displayOutcome{
-    //display if user is correct
+function displayOutcome() {
+  //display if user is correct
 }
-
-// Testing
